6163cf05fed438d4d5d2e728ad1a3dcebc675c2f
Dan Ash <imashdaniel@gmail.com>
2022-08-18 16:05:20 +0200

    remove old code for show/hide sections

--- public/script.js
+++ public/script.js
@@ -13,21 +13,3 @@ const observer = new IntersectionObserver(entries => {
 cards.forEach(card => {
   observer.observe(card)
 })
-
-/* Show/hide sections */
-const hiddenSection = document.querySelector('.hiddenSection');
-
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function handleClick() {
-  console.log('Clicked')
-  if (hiddenSection.style.display === 'none') {
-    hiddenSection.style.display = 'block';
-
-    btn.textContent = 'Hide div';
-  } else {
-    hiddenSection.style.display = 'none';
-
-    btn.textContent = 'Show div';
-  }
-});
